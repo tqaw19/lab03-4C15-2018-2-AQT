4ef3265e9a64e2f2247fa2b75238ceb0a0097300
Quesada <tqaw19@gmail.com>
2018-09-07 14:38:58 -0500

    local

--- src/main/java/com/tecsup/lab03/view/StudentReport/StudentReport.java
+++ src/main/java/com/tecsup/lab03/view/StudentReport/StudentReport.java
@@ -3,6 +3,6 @@ package com.tecsup.lab03.view.StudentReport;
 public class StudentReport {
 	 public static void main( String[] args )
 	    {
-	        System.out.println( "To commit merge to sprint-2" );
+	        System.out.println( "Merge sprint-2 & Master" );
 	    }
 }
