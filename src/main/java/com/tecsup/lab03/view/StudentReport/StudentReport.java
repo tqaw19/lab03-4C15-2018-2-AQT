dce42212515122d6b7c4cd323c4cfd5befa6fb95
Quesada <tqaw19@gmail.com>
2018-09-07 14:27:36 -0500

    commit solo_2_27

--- src/main/java/com/tecsup/lab03/view/StudentReport/StudentReport.java
+++ src/main/java/com/tecsup/lab03/view/StudentReport/StudentReport.java
@@ -3,6 +3,6 @@ package com.tecsup.lab03.view.StudentReport;
 public class StudentReport {
 	 public static void main( String[] args )
 	    {
-	        System.out.println( "Student Report test" );
+	        System.out.println( "to commit merge to sprint-2" );
 	    }
 }
