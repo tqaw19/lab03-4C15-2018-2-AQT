32cfcfc9154703631d03a77872836272670acc9e
Quesada <tqaw19@gmail.com>
2018-09-07 14:40:40 -0500

    local

--- src/main/java/com/tecsup/lab03/view/StudentReport/StudentReport.java
+++ src/main/java/com/tecsup/lab03/view/StudentReport/StudentReport.java
@@ -3,6 +3,6 @@ package com.tecsup.lab03.view.StudentReport;
 public class StudentReport {
 	 public static void main( String[] args )
 	    {
-	        System.out.println( "Merge sprint-2 & MasteR" );
+	        System.out.println( "Merge sprint-2 & MastER" );
 	    }
 }
