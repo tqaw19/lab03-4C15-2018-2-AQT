1c102cc1b21ce877ac83abb66fbf6ee2ce22a2db
Alumno <Alumno@LAB1509-22>
2018-09-07 14:01:42 -0500

    commit 14_01

---
+++ src/main/java/com/tecsup/lab03/controller/StudentController/StudentController.java
@@ -0,0 +1,8 @@
+package com.tecsup.lab03.controller.StudentController;
+
+public class StudentController {
+    public static void main( String[] args )
+    {
+        System.out.println( "Haciendo git student controller" );
+    }
+}
